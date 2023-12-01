4f0cd127400728b7dc6d0bd398dffa2be1a04b2f
changhak0310 <dlckdgkr0320@naver.com>
2023-12-02 01:33:07 +0900

    test: emotion 동적 스타일링 테스트

--- src/Button.tsx
+++ src/Button.tsx
@@ -1,6 +1,60 @@
-import * as React from "react";
+// import * as React from "react";
 import styles from "./Button.modules.css";
 
-export function Button({ title = "Title" }) {
-  return <button className={styles.button}>{title}</button>;
+// export function Button({ title = "Title" }) {
+//   return <button className={styles.button}>{title}</button>;
+// }
+
+import colors from "./constants/colors";
+//import { css } from "@emotion/react";
+
+interface ButtonInterface {
+  variant: 'primary' | 'secondary',
+  size: 'medium' | 'large',
+  title: string,
+  full: boolean,
 }
+
+export function Button({ variant='primary', size='medium', full=true, title="test", ...props }: ButtonInterface) {
+  const a = "width: 100%";
+    return (
+      <button
+        className={styles.button}
+        // `
+        //   ${full ? "width: 100%" : null};
+        //   ${TYPE_VARIANTS[variant]};
+        //   ${SIZE_VARIANTS[size]};
+        // `
+        {...props}
+      >{title}</button>
+    );
+  }
+  
+  const TYPE_VARIANTS = {
+    primary: {
+      color: colors.gray90,
+      backgroundColor: colors.primary70,
+      '&:hover': {
+        backgroundColor: colors.primary80,
+      },
+    },
+    secondary: {
+      color: colors.gray30,
+      backgroundColor: colors.gray90,
+      '&:hover': {
+        backgroundColor: colors.gray70,
+      },
+    },
+  };
+  
+  const SIZE_VARIANTS = {
+    medium: {
+      fontSize: '1rem',
+      padding: '0.75rem 1rem',
+    },
+    large: {
+      fontSize: '17px',
+      padding: '11px 22px',
+    },
+  };
+  
