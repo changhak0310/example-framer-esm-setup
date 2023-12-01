d8aefd5f5fe179cadbeca3790b37e62e283162bd
changhak0310 <dlckdgkr0320@naver.com>
2023-12-02 01:32:34 +0900

    feat: 색 추가

---
+++ src/constants/colors.ts
@@ -0,0 +1,19 @@
+const colors = {
+    gray10: "#1D1D1D ",
+    gray20: "#3B3B3B ",
+    gray30: "#5D5D5D ",
+    gray40: "#7B7B7B ",
+    gray50: "#989898", 
+    gray60: "#B5B5B5 ",
+    gray70: "#D4D4D4 ",
+    gray80: "#F2F2F2 ",
+    gray90: "#FFFFFF ",
+    primary10: "#FEF4F4",
+    primary20: "#FDE9E9",
+    primary70: "#E41C1C ",
+    primary80: "#C61818",
+    white: "#fff",
+    black: "#000"
+}
+
+export default colors;
